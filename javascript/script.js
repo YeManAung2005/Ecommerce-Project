27d837989952cf3f28ce45019abb0428c080fe34
YeManAung2005 <yemanaung222@gmail.com>
2023-09-01 18:24:53 +0630

    edited email vaildation

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,32 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
   close.addEventListener("click", () => {
     nav.classList.remove("active");
   });
+}
+
+function emailValidation() {
+  var emailField = document.getElementById("email-field");
+
+  if (
+    !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    emailField.style.borderColor = "red";
+    return false;
+  } else {
+    emailField.style.borderColor = "transparent";
+    return true;
+  }
+  function onKeyDown() {
+    document.getElementById("email-field").style.borderColor = "transparent";
+  }
 }
